78020187f8a3f92139863448a2628beab459b033
Tom B <tbensky@gmail.com>
2020-11-29 17:51:22 -0800

    monte carlo

---
+++ Labs/GammaRayMonteCarlo/functions.m
@@ -0,0 +1,25 @@
+function p = Pcs(E)
+    p = 10^(0.1*exp(-0.5*E)+0.02);
+end
+
+function p = Ppa(E)
+    p = 10^(132*exp(-28*E)+0.005); 
+end
+
+function ret = compton_scatter(E,theta)
+    alpha = E/0.511;
+    ret = E/(1+alpha*(1-cos(theta)));
+end
+
+function ret = P(E,theta)
+    alpha = E/0.511;
+    a = 1./(1+alpha.*(1-cos(theta)));
+    b = (1+cos(theta).^2)/2;
+    c = alpha^2*(1-cos(theta).^2);
+    d = (1+cos(theta).^2).*((1+alpha.*(1-cos(theta))));
+    ret = a.^2.*b.*(1+c./d);
+end
+
+function ret = smear(E)
+    ret = E+0.01*E*randn;
+end
